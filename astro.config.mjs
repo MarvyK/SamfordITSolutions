b012d7e685aced006b5ed9c540d7acfa8c371771
Marvin Poole <samfordsolutions@gmail.com>
2025-10-14 02:02:34 +0200

    Updated astro.config.mjs for GitHub pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://MarvyK.github.io", // Replace with your GitHub username
+  base: "/SamfordITSolutions", // Replace with your repository name (e.g., '/cageit-website')
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: "static",
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto",
+  },
+  vite: {
+    build: {
+      cssMinify: "lightningcss",
+    },
+  },
 });
